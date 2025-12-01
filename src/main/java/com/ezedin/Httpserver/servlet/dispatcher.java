11072f957e8a30123a70e54b659dce8489284cdf
Ezedin Ahmed <ezedinadefa@gmail.com>
2025-12-01 22:40:40 +0300

    scanning custom controllers

--- src/main/java/com/ezedin/Httpserver/servlet/dispatcher.java
+++ src/main/java/com/ezedin/Httpserver/servlet/dispatcher.java
@@ -2,11 +2,21 @@ package com.ezedin.Httpserver.servlet;
 
 import org.reflections.Reflections;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class dispatcher {
+    private final Map<String, Method> routeMethods = new HashMap<>();
+    private final Map<String, Object> controllers = new HashMap<>();
+
+    public dispatcher(String basePackage) {
+        scanController(basePackage);
+    }
     public String dispatch(String httpMethod,String path){
+
     return "";
     }
     private void scanController(String basePackage){
@@ -16,8 +26,31 @@ public class dispatcher {
             if(!clazz.isAnnotationPresent(customController.class)) continue;
             Object instance = null;
             for(Method method : clazz.getDeclaredMethods()){
-                if(method.isAnnotationPresent(customGet.class)){}
+                if(method.isAnnotationPresent(customGet.class)){
+                    if(instance == null) instance = createInstance(clazz);
+                    String path = method.getAnnotation(customGet.class).value();
+                    String key = "GET: "+path;
+                    routeMethods.put(key,method);
+                    controllers.put(key,instance);
+                }
+                if (method.isAnnotationPresent(customPost.class)) {
+                    if (instance == null) instance = createInstance(clazz);
+
+                    String path = method.getAnnotation(customPost.class).value();
+                    String key = "POST:" + path;
+                    routeMethods.put(key, method);
+                    controllers.put(key, instance);
+                }
             }
+        }
+    }
+    private Object createInstance(Class<?> cls){
+        try{
+            Constructor<?> constructor = cls.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        }catch(Exception e){
+            throw new RuntimeException(e);
         }
     }
 }
