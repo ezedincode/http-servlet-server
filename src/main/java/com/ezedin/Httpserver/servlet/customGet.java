9c48ac9db526eaba048dc0ca4ab3b61d41d3e695
Ezedin Ahmed <ezedinadefa@gmail.com>
2025-12-01 22:03:53 +0300

    creating custom get and post annotation

---
+++ src/main/java/com/ezedin/Httpserver/servlet/customGet.java
@@ -0,0 +1,12 @@
+package com.ezedin.Httpserver.servlet;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface customGet {
+    String value();
+}
